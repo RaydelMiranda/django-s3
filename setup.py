7f5466b85e09cee53a94868c70399ca56a00f9ca
Raydel Miranda <raydel.miranda.gomez@gmail.com>
2017-06-16 10:32:12 -0400

    Renaming the project, the name already exists en pypi.

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
         'Topic :: Software Development',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: GPL',
+        'License :: GPL',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
