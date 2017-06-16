2647090378d01e5fb6b3b2b03c14047ae6f41e6d
Raydel Miranda <raydel.miranda.gomez@gmail.com>
2017-06-16 09:43:30 -0400

    Modified setup.py

--- setup.py
+++ setup.py
@@ -59,10 +59,9 @@ setup(
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
         'Topic :: Software Development',
-        'Environment :: Web Environment',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GPL',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
