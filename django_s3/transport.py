b4bfe955a9dc29e1ee04a76a1ad3c1a746375fd2
Raydel Miranda <raydel.miranda.gomez@gmail.com>
2017-06-22 12:27:16 -0400

    Fixing bugs related names.

--- django_s3/transport.py
+++ django_s3/transport.py
@@ -56,11 +56,11 @@ class Transport(object):
         """
         try:
             key_holder = Key(self.__bucket)
-            key_holder.key = resource.name
+            key_holder.key = "{}/{}".format(settings.S3_CATGORY_MAP[resource.code], resource.name)
             key_holder.set_contents_from_filename(os.path.join(django_s3_settings.S3_UPLOAD_DIR_PATH, resource.name))
             key_holder.make_public()
         except Exception as err:
-            Transport.logger(_("Error uploading file: {}. Error: {}".format(resource.name, err)))
+            Transport.logger.error(_("Error uploading file: {}. Error: {}".format(resource.name, err)))
             # Right now we don't know what exceptions are expected here, we propagate the error
             # up. If we found some exception then we'll add the proper handler.
             raise
@@ -79,16 +79,16 @@ class Transport(object):
 
         # If the file exists do not download again.
         if not os.path.exists(filename):
-            Transport.logger(_('Downloading {} to {}.'.format(resource.name, filename)))
+            Transport.logger.info(_('Downloading {} to {}.'.format(resource.name, filename)))
             try:
                 self.__files.get_contents_to_filename(filename)
             except Exception as err:
-                Transport.logger(_("Error downloading file: {}. Error: {}".format(resource.name, err)))
+                Transport.logger.error(_("Error downloading file: {}. Error: {}".format(resource.name, err)))
                 # Right now we don't know what exceptions are expected here, we propagate the error
                 # up. If we found some exception then we'll add the proper handler.
                 raise
         else:
-            Transport.logger(_('File already exists, skipping download: {}'.format(filename)))
+            Transport.logger.info(_('File already exists, skipping download: {}'.format(filename)))
 
         return filename
 
