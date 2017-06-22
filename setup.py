f895420f14f8e9507147c061542f6aaf36b297ad
Raydel Miranda <raydel.miranda.gomez@gmail.com>
2017-06-22 02:06:39 -0400

    Creating a new version distribution.

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ except(IOError, ImportError):
 setup(
     name='real-django-s3',
 
-    version='1.1',
+    version='1.2',
     description='A simple API for download and upload graphics resources to/from a Amazon S3 bucket',
     long_description=long_description,
 
