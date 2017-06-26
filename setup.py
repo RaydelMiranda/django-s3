c973ac988c8542e757d9c7dacafffff2733d36b4
Raydel Miranda <raydel.miranda.gomez@gmail.com>
2017-06-26 13:59:07 -0400

    Updating the version

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ except(IOError, ImportError):
 setup(
     name='real-django-s3',
 
-    version='1.2.7',
+    version='1.2.8',
     description='A simple API for download and upload graphics resources to/from a Amazon S3 bucket',
     long_description=long_description,
 
